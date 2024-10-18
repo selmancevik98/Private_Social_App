2a69a72d2cec9127432494cc2eaf55eba8a75b10
selmancevik98 <selmancevik21@gmail.com>
2024-10-18 17:44:05 +0300

    Development stage

--- app/src/main/java/com/hebun/private_social_app/LoginActivity.java
+++ app/src/main/java/com/hebun/private_social_app/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.hebun.private_social_app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
+    TextInputEditText login_mail_edittext, login_password_edittext;
+    TextView login_forgot_password, login_signup_redirect;
+    Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,15 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        definitions();
+    }
+
+    private void definitions() {
+        login_mail_edittext = findViewById(R.id.login_mail_edittext);
+        login_password_edittext = findViewById(R.id.login_password_edittext);
+        login_forgot_password = findViewById(R.id.login_forgot_password);
+        login_signup_redirect = findViewById(R.id.login_signup_redirect);
+        login_btn = findViewById(R.id.login_btn);
     }
 }
