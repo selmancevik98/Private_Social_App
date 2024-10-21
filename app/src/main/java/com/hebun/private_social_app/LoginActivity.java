5dc41f633595dcd716ea1af847730a5b7ebd796d
selmancevik98 <selmancevik21@gmail.com>
2024-10-21 14:27:36 +0300

    Development stage

--- app/src/main/java/com/hebun/private_social_app/LoginActivity.java
+++ app/src/main/java/com/hebun/private_social_app/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.hebun.private_social_app;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -27,7 +30,6 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         definitions();
     }
 
@@ -37,5 +39,27 @@ public class LoginActivity extends AppCompatActivity {
         login_forgot_password = findViewById(R.id.login_forgot_password);
         login_signup_redirect = findViewById(R.id.login_signup_redirect);
         login_btn = findViewById(R.id.login_btn);
+
+        login_forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setTitle("Forgot password");
+                builder.setMessage("Enter your registered e-mail address in the application. We will send a password reset link to your email address.");
+                EditText editText = new EditText(view.getContext());
+
+                editText.setHint("e-mail");
+
+
+                builder.create().show();
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
