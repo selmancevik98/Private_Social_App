8afba1934ad596b18fdb6d3766600246f54f50d3
Hebun <selmancevik21@gmail.com>
2024-10-23 23:12:16 +0300

    Development stage

--- app/src/main/java/com/hebun/private_social_app/MainActivity.java
+++ app/src/main/java/com/hebun/private_social_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hebun.private_social_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -20,5 +21,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
     }
 }
