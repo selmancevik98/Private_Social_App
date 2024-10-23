4806cf2eec50f0f4ab9ffdaaf1953fb078661443
selmancevik98 <selmancevik21@gmail.com>
2024-10-23 21:34:36 +0300

    Development stage

--- app/src/main/java/com/hebun/private_social_app/SignupActivity.java
+++ app/src/main/java/com/hebun/private_social_app/SignupActivity.java
@@ -1,6 +1,10 @@
 package com.hebun.private_social_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class SignupActivity extends AppCompatActivity {
+    TextInputEditText signup_mail_edittext, signup_password_edittext;
+    Button signup_btn;
+    TextView signup_login_redirect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,31 @@ public class SignupActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        definitions();
+    }
+
+    private void definitions () {
+        signup_btn = findViewById(R.id.signup_btn);
+        signup_login_redirect = findViewById(R.id.signup_login_redirect);
+        signup_mail_edittext = findViewById(R.id.signup_mail_edittext);
+        signup_password_edittext = findViewById(R.id.signup_password_edittext);
+
+        signup_login_redirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finishAffinity();
+            }
+        });
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
         });
     }
 }
