eea0372c0be822d35272e3fa31bd3bbce2930620
selmancevik98 <selmancevik21@gmail.com>
2024-11-08 21:52:27 +0300

    Development stage

--- app/src/main/java/com/hebun/private_social_app/adapters/Adapter_Chats.java
+++ app/src/main/java/com/hebun/private_social_app/adapters/Adapter_Chats.java
@@ -1,4 +1,43 @@
 package com.hebun.private_social_app.adapters;
 
-public class Adapter_Chats {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hebun.private_social_app.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Adapter_Chats extends RecyclerView.Adapter<Adapter_Chats.ViewHolderC> {
+
+    @NonNull
+    @Override
+    public ViewHolderC onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderC holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolderC extends RecyclerView.ViewHolder {
+        CircleImageView chats_profile_image;
+        TextView chats_name_text, chats_last_msg, chats_date;
+        public ViewHolderC(@NonNull View itemView) {
+            super(itemView);
+            chats_date = itemView.findViewById(R.id.chats_date);
+            chats_profile_image = itemView.findViewById(R.id.chats_profile_image);
+            chats_name_text = itemView.findViewById(R.id.chats_name_text);
+            chats_last_msg = itemView.findViewById(R.id.chats_last_msg);
+        }
+    }
 }
